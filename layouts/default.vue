1796e68df63819742db7d8b7c1d83dc60e719cbf
ณัฐพล บุญมี <p.natapol.b@gmail.com>
2019-11-13 09:50:37 +0700

    file commit

---
+++ layouts/default.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app >
+    <v-toolbar  flat app color="#6bb83c">
+      <v-spacer></v-spacer>
+      <v-toolbar-items >
+        <v-btn class="white--text" depressed text color="#6bb83c">เข้าสู่ระบบ</v-btn>
+        <v-btn class="white--text" depressed text color="#6bb83c">สมัครสมาชิก</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <v-card class="d-flex pa-2" outlined tile color="#6bb83c">
+
+      </v-card>
+    </v-content>
+
+    <v-content>
+
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return{
+
+    }
+  },
+  components:{
+
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-container fill-height>
+    <v-layout justify-center align-center>
+      <v-flex shrink>
+        <v-btn icon color="test" large >
+          <v-icon>mdi-theme-light-dark</v-icon>
+        </v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+
+  }
+}
+</script>
